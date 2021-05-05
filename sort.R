e29b105f7c8f019143b69a60c2792950a1f7ff39
dvthang774 <74769024+dvthang774@users.noreply.github.com>
2021-05-05 13:37:41 +0700

    sang 5/5

--- sort.R
+++ sort.R
@@ -1,3 +1,4 @@
+#selfsort
 selfsort <- function(x){
         if(length(x)>1){
                 min<-which.min(x)
@@ -173,3 +174,33 @@ heap=heap.building(vec)
 heap_sort=heap.sort(heap)
 heap_sort
 ;
+
+#Merger sort:
+mmerge<-function(a,b) {
+        r<-numeric(length(a)+length(b))
+        ai<-1; bi<-1; j<-1;
+        for(j in 1:length(r)) {
+                if((ai<=length(a) && a[ai]<b[bi]) || bi>length(b)) {
+                        r[j] <- a[ai] 
+                        ai <- ai+1
+                        } else {
+                                r[j] <- b[bi] 
+                                bi <- bi+1
+                        }
+                }
+        r
+        }
+mmergesort<-function(A) {
+        if(length(A)>1) {
+                q <- ceiling(length(A)/2)
+                a <- mmergesort(A[1:q])
+                b <- mmergesort(A[(q+1):length(A)])
+                mmerge(a,b)
+                } else {
+                        A
+                }
+        }
+x <-sample(1:100,10)
+
+RES <- mmergesort(x)
+RES
